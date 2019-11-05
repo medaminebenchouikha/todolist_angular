5915e1c15d43d4022ac6eda2bde3836b9c08e0f9
MDABC <med.amine.ben.chouikha@gmail.com>
2019-11-05 22:17:24 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TodoListComponent } from './components/user/todo-list/todo-list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { TodoAddComponent } from './components/user/todo-add/todo-add.component';
+import { TodoUpdateComponent } from './components/user/todo-update/todo-update.component';
+import { UserGuard } from './guards/user.guard';
+import { UserListComponent } from './admin/user-list/user-list.component';
+import { AdminGuard } from './guards/admin.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //homeroute
+  {
+    path:'',
+    component:HomeComponent,
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+      path:'user/todo-list',
+      component:TodoListComponent,
+      canActivate:[UserGuard]
+    },
+    {
+      path:'user/todo-add',
+      component:TodoAddComponent,
+      canActivate:[UserGuard]
+    },
+    {
+      path:'user/todo-update/:id',
+      component:TodoUpdateComponent,
+      canActivate:[UserGuard]
+    },
+    {
+      path:'admin/user-list',
+      component:UserListComponent,
+      canActivate:[AdminGuard]
+    },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TodoListComponent } from './components/user/todo-list/todo-list.component';
+import { TodoAddComponent } from './components/user/todo-add/todo-add.component';
+import { TodoUpdateComponent } from './components/user/todo-update/todo-update.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxCaptchaModule } from 'ngx-captcha';
+import {
+  ToastrModule,
+  ToastNoAnimation,
+  ToastNoAnimationModule
+} from 'ngx-toastr';
+import { TodoService } from './services/todo.service';
+import { UserListComponent } from './admin/user-list/user-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileSelectDirective } from 'ng2-file-upload';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    TodoListComponent,
+    TodoAddComponent,
+    TodoUpdateComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserListComponent,
+    FileSelectDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastNoAnimationModule.forRoot(),
+    NgxCaptchaModule
+    ],
+  providers: [
+    UserService,
+    TodoService
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
